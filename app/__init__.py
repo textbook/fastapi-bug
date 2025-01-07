ce6c38354efd6f90d7eae51d68216064f7fcf202
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2025-01-07 23:50:58 +0000

    Propagate header in exception handler

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from http import HTTPStatus
 from typing import Optional
 
-from fastapi import Depends, FastAPI, Header, HTTPException, Response
+from fastapi import Depends, FastAPI, Header, HTTPException, Request, Response
+from fastapi.exception_handlers import http_exception_handler
+from starlette.exceptions import HTTPException as StarletteHTTPException
 from typing_extensions import Annotated
 
 
@@ -12,7 +14,19 @@ def request_id(
     if request_correlation_id is not None:
         response.headers.append("Request-Correlation-Id", request_correlation_id)
 
+
 app = FastAPI(dependencies=[Depends(request_id)])
+
+
+@app.exception_handler(StarletteHTTPException)
+async def _(request: Request, exc: StarletteHTTPException):
+    if exc.headers is None:
+        exc.headers = dict()
+    if (
+        request_correlation_id := request.headers.get("Request-Correlation-Id")
+    ) is not None:
+        exc.headers["Request-Correlation-Id"] = request_correlation_id
+    return await http_exception_handler(request, exc)
 
 
 @app.get("/success")
